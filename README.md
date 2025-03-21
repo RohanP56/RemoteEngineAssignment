3f1803c19cf9f7315e9a64bdf3bf7b5caaa5fb83
Rohan <devx.rohan@gmail.com>
2025-03-21 23:33:41 +0530

    Add images and update image paths in Imagecomp component

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Remote Engine Assignment
 - Here, i have choosen the layout-1 from the given layouts.
 - Here, I have use HTML, CSS, Bootstarp and React to build this landing page. Which is functional as well as responsive.
+
+https://remote-engine-assignment.vercel.app/
--- src/components/Imagecomp.jsx
+++ src/components/Imagecomp.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import "./Imagecomp.css";
-
+//import img from "../../public/image"
 function Imagecomp() {
   return (
     <div className="gallery-section">
       {/* First row */}
       <div className="gallery-row">
         <img
-          src="../image/1.png"
+          src="/image/1.png"
           className="rounded"
           alt="Scenic landscape 1"
         />
         <img
-          src="../image/2.png"
+          src="/image/2.png"
           className="rounded"
           alt="Scenic landscape 2"
         />
         <img
-          src="../image/3.png"
+          src="/image/3.png"
           className="rounded"
           alt="Scenic landscape 3"
         />
         <img
-          src="../image/4.png"
+          src="/image/4.png"
           className="rounded"
           alt="Scenic landscape 4"
         />
@@ -31,22 +31,22 @@ function Imagecomp() {
       {/* Second row */}
       <div className="gallery-row">
         <img
-          src="../image/5.png"
+          src="/image/5.png"
           className="rounded"
           alt="Scenic landscape 5"
         />
         <img
-          src="../image/6.png"
+          src="/image/6.png"
           className="rounded"
           alt="Scenic landscape 6"
         />
         <img
-          src="../image/7.png"
+          src="/image/7.png"
           className="rounded"
           alt="Scenic landscape 7"
         />
         <img
-          src="../image/8.png"
+          src="/image/8.png"
           className="rounded"
           alt="Scenic landscape 8"
         />
